5627c668409aa685c74e7da693691dc035cb42e8
anntreesa <anntreesa@qburst.com>
2018-07-25 11:29:37 +0530

    first commit

---
+++ src/main/java/com/qburst/maven_derived/MyApp.java
@@ -0,0 +1,15 @@
+package com.qburst.maven_derived;
+
+
+import com.qburst.maven_base.*;
+
+
+public class MyApp 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println(App.findSum(3, 4) );
+    	App.first();
+    	System.out.println("This is from second program");
+    }
+}
